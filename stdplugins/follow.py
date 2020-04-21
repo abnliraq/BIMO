911597734aa2b37a06d8caec37857481ba7ba4ae
nazkun <55693311+nazkun@users.noreply.github.com>
2020-04-21 23:58:33 +0200

    Update follow.py

--- stdplugins/follow.py
+++ stdplugins/follow.py
@@ -3,10 +3,9 @@ from telethon import events, functions, __version__
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="helpme ?(.*)", allow_sudo=True))  # pylint:disable=E0602
+@borg.on(admin_cmd(pattern="follow ?(.*)", allow_sudo=True))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
-    await event.edit("processing...")
-    str = event.pattern_match.group(1)
-            await event.edit("**" + str + "**")
+    input_str = event.pattern_match.group(1)
+    await event.edit(input_str)
