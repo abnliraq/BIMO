5cbdcf5f472b17a417eed6187db6402915342d24
nazkun <55693311+nazkun@users.noreply.github.com>
2020-04-22 02:19:40 +0200

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -23,22 +23,7 @@ async def _(event):
     if splugin_name in borg._plugins:
         s_help_string = borg._plugins[splugin_name].__doc__
     else:
-        help_string = "nazi"
-        await event.edit(splugin_name)
-    tgbotusername = Config.TG_BOT_USER_NAME_BF_HER  # pylint:disable=E0602
-    if tgbotusername is not None:
-        results = await borg.inline_query(  # pylint:disable=E0602
-            tgbotusername,
-            help_string + "\n\n" + s_help_string
-        )
-        await results[0].click(
-            event.chat_id,
-            reply_to=event.reply_to_msg_id,
-            hide_via=True
-        )
-        await event.delete()
-    else:
-        await event.reply(help_string + "\n\n" + s_help_string)
+        await event.reply(splugin_name)
         await event.delete()
 
 
